ea9e946d4525d0bae894fcb1f4183d6131f212a8
Sumit Lasiwa <lasiwasumit@gmail.com>
2026-04-22 19:11:45 +0545

    first commit

---
+++ js/script.js
@@ -0,0 +1,65 @@
+// toggle icon navbar
+let menuIcon = document.querySelector('#menu-icon');
+let navbar = document.querySelector('.navbar');
+
+menuIcon.onclick = () => {
+    menuIcon.classList.toggle('bx-x');
+    navbar.classList.toggle('active');
+}
+
+// scroll section active link 
+
+let sections = document.querySelectorAll('section')
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    sections.forEach(sec => {
+        let top = window.scrollY;
+        let offset = sec.offsetTop -150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if(top >= offset && top < offset + height){
+            navLinks.forEach(links => {
+                links.classList.remove('active');
+                document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
+
+    // sticky navbar
+    let header = document.querySelector('header');
+
+    header.classList.toggle('sticky', window.scrollY > 100);
+
+    // remove toggle icon and navbar when click navbar link (scroll)
+    menuIcon.classList.remove('bx-x');
+    navbar.classList.remove('active');
+};
+
+function sendMail(event){
+    event.preventDefault(); // stop form reload
+
+    const name = document.querySelector('input[placeholder="Full Name"]').value;
+    const email = document.querySelector('input[placeholder="Email Address"]').value;
+    const mobile = document.querySelector('input[placeholder="Mobile Number"]').value;
+    const subject = document.querySelector('input[placeholder="Email Subject"]').value;
+    const message = document.querySelector('textarea').value;
+
+    const params = {
+        from_name: name,
+        from_email: email,
+        mobile: mobile,
+        subject: subject,
+        message: message
+    };
+
+    emailjs.send("service_b1ziifo", "template_pqa91dv", params)
+        .then(() => {
+            alert("Your Email has been sent!");
+        })
+        .catch((err) => {
+            console.error("Failed to send email:", err);
+            alert("Oops! Something went wrong.");
+        });
+}
